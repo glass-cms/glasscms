29d75cd74372f401c45e55b1bc3e755ce563e9f8
Maikel Veen <veenmaikel@gmail.com>
2024-05-06 08:58:19 +0200

    feat: Implement FileSystemSourcer

--- cmd/parse.go
+++ cmd/parse.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/glass-cms/glasscms/sourcer"
 	"github.com/spf13/cobra"
 )
 
@@ -13,16 +14,21 @@ type ParseCommand struct {
 func NewParseCommand() *ParseCommand {
 	c := &ParseCommand{}
 	c.Command = &cobra.Command{
-		Use:   "parse",
+		Use:   "parse <source>",
 		Short: "Parses source files",
 		Long:  "Parses source files and pumps them to the desired destination",
 		RunE:  c.Execute,
+		Args:  cobra.ExactArgs(1),
 	}
 
 	return c
 }
 
 func (c *ParseCommand) Execute(cmd *cobra.Command, args []string) error {
-	fmt.Println("Parsing source files")
-	return nil
+	path := args[0]
+
+	source, err := sourcer.NewFileSystemSourcer(path)
+	fmt.Println(source.Size())
+
+	return err
 }
--- sourcer/fs.go
+++ sourcer/fs.go
@@ -1,21 +1,62 @@
 package sourcer
 
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 var _ DataSourcer = &FileSystemSourcer{}
 
-type FileSystemSourcer struct{}
+// FileSystemSourcer is a DataSourcer that reads files from the file system.
+type FileSystemSourcer struct {
+	files  []string
+	cursor int // cursor is the index of the next file to be read
+}
 
-func NewFileSystemSourcer() *FileSystemSourcer {
-	return &FileSystemSourcer{}
+// NewFileSystemSourcer creates a new FileSystemSourcer.
+func NewFileSystemSourcer(rootPath string) (*FileSystemSourcer, error) {
+	var files []string
+
+	err := filepath.WalkDir(rootPath, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() && strings.HasSuffix(d.Name(), ".md") {
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileSystemSourcer{
+		files: files,
+	}, nil
 }
 
 func (s *FileSystemSourcer) Next() (string, error) {
-	return "", nil
+	if s.cursor >= len(s.files) {
+		return "", ErrDone
+	}
+
+	data, err := os.ReadFile(s.files[s.cursor])
+	s.cursor++
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
 }
 
 func (s *FileSystemSourcer) Remaining() int {
-	return 0
+	return s.Size() - s.cursor
 }
 
 func (s *FileSystemSourcer) Size() int {
-	return 0
+	return len(s.files)
 }
