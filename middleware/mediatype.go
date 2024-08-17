3cd9cbb40009a699b197d3c6ca3ef2f61da7a3e0
Maikel Veen <veenmaikel@gmail.com>
2024-08-17 11:13:40 +0200

    feat: Add MediaType middleware function

---
+++ middleware/mediatype.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+	"slices"
+
+	"github.com/glass-cms/glasscms/mediatype"
+)
+
+// MediaType generates a handler that writes a 415 Unsupported Media Type header
+// if the request's Content-Type header does not match the provided media type.
+func MediaType(accepted ...string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mdt, err := mediatype.Parse(r.Header.Get("Content-Type"))
+			if err != nil {
+				http.Error(w, "Invalid Media Type", http.StatusBadRequest)
+				return
+			}
+
+			if !slices.Contains(accepted, mdt.MediaType) {
+				http.Error(w, "Unsupported Media Type", http.StatusUnsupportedMediaType)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
