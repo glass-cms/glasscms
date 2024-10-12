aed1f4d1f4114acae40220ddef179fe1cfce28eb
Maikel Veen <veenmaikel@gmail.com>
2024-10-12 17:50:45 +0200

    feat: Add Transactionally to the Repository interface

--- internal/item/repository/repository.go
+++ internal/item/repository/repository.go
@@ -29,6 +29,39 @@ func NewRepository(db *sql.DB, errorHandler database.ErrorHandler) *ItemReposito
 	}
 }
 
+// Transactionally executes a function within a database transaction. It commits the transaction
+// if the function succeeds, otherwise it rolls back. If rollback fails, both errors are returned.
+func (r *ItemRepository) Transactionally(ctx context.Context, f func(tx *sql.Tx) error) (err error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			if rbErr := tx.Rollback(); rbErr != nil {
+				err = fmt.Errorf("panic occurred: %v, rollback error: %w", p, rbErr)
+			} else {
+				err = fmt.Errorf("panic occurred: %v", p)
+			}
+		}
+	}()
+
+	err = f(tx)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("transaction rollback error: %w, original error: %w", rbErr, err)
+		}
+		return err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // CreateItem creates a new item in the database.
 // If a transaction is provided, the item will be created within the transaction.
 func (r *ItemRepository) CreateItem(ctx context.Context, tx *sql.Tx, item *item.Item) error {
