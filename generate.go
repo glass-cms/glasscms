5ccc98154da82dfae9762239bda6b71f5789e83c
Maikel Veen <veenmaikel@gmail.com>
2024-11-08 21:44:03 +0100

    chore: Generate client package

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
 package main
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=oapi-codegen-config.yaml openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=oapi-server.yaml openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=oapi-client.yaml openapi.yaml
