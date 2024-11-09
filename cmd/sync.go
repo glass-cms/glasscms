f0ff974161221e326879da8d5343f77baffa43ce
Maikel Veen <veenmaikel@gmail.com>
2024-11-09 17:00:19 +0100

    feat: Add stubbed Syncer component

--- cmd/sync.go
+++ cmd/sync.go
@@ -3,13 +3,17 @@ package cmd
 import (
 	"errors"
 
-	"github.com/glass-cms/glasscms/pkg/log"
+	"github.com/glass-cms/glasscms/internal/sourcer"
+	"github.com/glass-cms/glasscms/internal/sourcer/fs"
+	"github.com/glass-cms/glasscms/internal/sync"
+	"github.com/glass-cms/glasscms/pkg/client"
 	"github.com/spf13/cobra"
 )
 
 const (
 	ArgSourceType = "source-type"
 	ArgLiveMode   = "live"
+	ArgServerURL  = "server"
 )
 
 type SyncCommand struct {
@@ -19,8 +23,8 @@ type SyncCommand struct {
 }
 
 type SyncCommandOptions struct {
-	SourceType string
-	LiveMode   bool
+	LiveMode  bool
+	ServerURL string
 }
 
 // NewSyncCommand returns a new sync command.
@@ -33,32 +37,61 @@ func NewSyncCommand() *SyncCommand {
 		Use:   "sync",
 		Short: "Synchronize items from a source to the server",
 		RunE:  cmd.RunE,
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(2),
 	}
 
 	flagset := cmd.Command.Flags()
 
-	flagset.StringVar(&cmd.opts.SourceType, ArgSourceType, "",
-		"The source type to synchronize items from")
 	flagset.BoolVar(&cmd.opts.LiveMode, ArgLiveMode, false,
 		"When live mode is enabled, items are synchronized to the server, otherwise changes are only previewed")
+
+	flagset.StringVar(&cmd.opts.ServerURL, ArgServerURL, "http://localhost:8080",
+		"The URL of the server to synchronize items to")
 
 	return cmd
 }
 
-func (c *SyncCommand) RunE(cmd *cobra.Command, _ []string) error {
-	ctx := cmd.Context()
-
-	logger, err := log.NewLogger()
+func (c *SyncCommand) RunE(cmd *cobra.Command, args []string) error {
+	// Setup dependencies.
+	sr, err := c.initSourcer(args)
 	if err != nil {
 		return err
 	}
 
-	if c.opts.SourceType == "" {
-		return errors.New("source type is required")
+	cl, err := client.NewClient(c.opts.ServerURL)
+	if err != nil {
+		return err
 	}
 
-	// Print the source type and live mode.
-	logger.InfoContext(ctx, "Synchronizing items", "sourceType", c.opts.SourceType, "liveMode", c.opts.LiveMode)
-	return nil
+	// Create a new syncer and synchronize items.
+	syncer, err := sync.NewSyncer(&sr, cl)
+	if err != nil {
+		return err
+	}
+
+	return syncer.Sync(cmd.Context(), c.opts.LiveMode)
+}
+
+// initSourcer initializes a sourcer based on the provided arguments.
+// The first argument specifies the source type, and subsequent arguments are source-specific parameters.
+// Returns an error if the source type is unrecognized or missing.
+func (c *SyncCommand) initSourcer(args []string) (sourcer.Sourcer, error) {
+	sourceTypeArg := args[0]
+	if sourceTypeArg == "" {
+		return nil, errors.New("source type is required")
+	}
+
+	sourceType, ok := sourcer.SourceTypeValue[sourceTypeArg]
+	if !ok {
+		return nil, errors.New("unrecognized source type")
+	}
+
+	switch sourceType {
+	case sourcer.SourceTypeUnspecified:
+		return nil, errors.New("source type is required")
+	case sourcer.SourceTypeFilesystem:
+		return fs.NewSourcer(args[1])
+	}
+
+	return nil, errors.New("unrecognized source type")
 }
--- internal/sync/sync.go
+++ internal/sync/sync.go
@@ -1,2 +1,37 @@
 // Package sync provides a way to synchronize items from a source to the server.
 package sync
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/glass-cms/glasscms/internal/sourcer"
+	"github.com/glass-cms/glasscms/pkg/client"
+	"github.com/glass-cms/glasscms/pkg/log"
+)
+
+// Syncer synchronizes items from a source to the server.
+type Syncer struct {
+	sourcer *sourcer.Sourcer
+	client  *client.Client
+	logger  *slog.Logger
+}
+
+// NewSyncer returns a new syncer.
+func NewSyncer(s *sourcer.Sourcer, c *client.Client) (*Syncer, error) {
+	logger, err := log.NewLogger()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Syncer{
+		sourcer: s,
+		client:  c,
+		logger:  logger,
+	}, nil
+}
+
+// Sync synchronizes items from a source to the server.
+func (s *Syncer) Sync(_ context.Context, _ bool) error {
+	return nil
+}
